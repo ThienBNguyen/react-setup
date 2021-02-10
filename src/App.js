6c4802c6b526adc1475fa4f0151bd41c2e50b7d1
Thien Nguyen <thien.nguyen30191@gmail.com>
2021-02-10 15:03:52 -0600

    setup react js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <button class="btn btn-primary" type="button">
+        <i class="fas fa-search fa-sm">jkdfndgklsjd</i>
+      </button>
+    </div>
+  );
+}
+
+export default App;
